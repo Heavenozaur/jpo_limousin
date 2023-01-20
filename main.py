a6e81bfb26ae7d238406629d93700087b6165564
p_brnx <paul.biarneix@gmail.com>
2023-01-21 00:06:43 +0100

    game finished

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ import random
 dim_x = 960
 dim_y = 720
 
-score_joueur = 0
-
 game = Tk()
-game.title('Clément snake')
+game.title('Dino JPO')
+
 
 frame = Canvas(game,width = dim_x, height = dim_y, bg = 'white')
 frame.grid(row=1,column=0,columnspan=3)
-
-text = Text(frame, height=10, width=50)
-text.insert(INSERT, "Hello.....")
 
 dino1 = PhotoImage(file='dino1.png')
 dino1 = dino1.zoom(25)
@@ -48,16 +44,19 @@ cactus4 = cactus4.subsample(5)
 Dino = frame.create_image(0,500, image = dino1, anchor='nw')
 Sol = frame.create_image(500,670, image = sol)
 Cactus = frame.create_image(1300,530, image = cactus)
+Score = frame.create_text((800,50),text="0", font=('Courier 25'))
 
 
 photoDino = dino1
-
+score_joueur = 0
+vitesse = -10
 bool = False
 
 
 def next_image(event=None):
+    vitesse_changement()
     frame.move(Sol, -10, 0)
-    frame.after(10, next_image)
+    frame.after(vitesse, next_image)
     x,y = frame.coords(Sol)
     if x<0 : 
         frame.move(Sol, 1000, 0)
@@ -76,49 +75,60 @@ def apparition_cactus() :
 
 
 def move_cactus() :
+    vitesse_changement()
     frame.move(Cactus, -10, 0)
     x,y = frame.coords(Cactus)
     x1,y1= frame.coords(Dino)
     collision = frame.find_overlapping(x1=frame.bbox(Cactus)[0],y1=frame.bbox(Cactus)[1],x2=frame.bbox(Cactus)[2],y2=frame.bbox(Cactus)[3])
-    print(collision)
     if x==-100 :
         apparition_cactus()
     elif len(collision) == 3:
         game.quit()
-
-    frame.after(10, move_cactus)
+    frame.after(vitesse, move_cactus)
 
 def dinoMonte(event=None) : 
     if frame.coords(Dino)[1] >= 300 :
         frame.move(Dino, 0, -10)
-        frame.after(5, dinoMonte)
+        frame.after(10, dinoMonte)
     else :
         dinoDescend()
 
 def dinoDescend() :
     if frame.coords(Dino)[1] != 500 :
         frame.move(Dino, 0, 10)
-        frame.after(5, dinoDescend)
+        frame.after(10, dinoDescend)
 
 def dinoCourt() :
     global photoDino
     if photoDino == dino1 : 
         photoDino = dino2
     else :
-        photoDino = dino1
+        photoDino = dino1         
     frame.itemconfigure(Dino,image=photoDino)  
     frame.after(100, dinoCourt)
     
-def score_joueur() : 
-    score+=1
-    frame.after(100, score_joueur)
+def incremente_score_joueur() : 
+    global score_joueur
+    score_joueur+=1
+    frame.itemconfigure(Score,text="Score : "+str(score_joueur))  
+    frame.after(100, incremente_score_joueur)
 
+def vitesse_changement() :
+    global vitesse
+    print(vitesse)
+    if score_joueur < 99 :
+        vitesse =  10
+    elif score_joueur > 99 :
+        vitesse = 10 - int(score_joueur/100)
+    elif score_joueur > 999 :
+        vitesse = 0 - int(score_joueur/1000)    
 
 game.after_idle(move_cactus)
 game.after_idle(next_image)
 game.after_idle(dinoCourt)
+game.after_idle(incremente_score_joueur)
 
-game.bind("<KeyPress-z>",dinoMonte)
 
-text.pack()
+game.bind("<KeyPress-space>",dinoMonte)
+
 game.mainloop()
